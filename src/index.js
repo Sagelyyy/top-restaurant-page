fc21524b4eae7cc4224f93ef0b35cd8e1e15f31e
Chris <orj.tsuk@gmail.com>
2021-08-06 08:29:06 -0400

    Getting the basic site setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+import './style.css'
+import splash from './images/pizza-checker.jpg'
+
+function componenet(){
+    const content = document.querySelector('#content')
+
+    const myImage = new Image()
+    myImage.src = splash
+
+    content.appendChild(myImage)
+}
+
+document.body.appendChild(componenet())
